c53a868bbbb2b88abc1361d2130720012b2cb5de
OlaleyeAg20 <101418565+OlaleyeAg20@users.noreply.github.com>
2023-07-19 19:25:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const numberIndex = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","-","+","&","?","!","*",";",":","@","#","_","(",")","%"]
+const darkBtn = document.getElementById("dark")
+const lightBtn = document.getElementById("light")
 let output = document.getElementById("password")
 let button = document.getElementById("btn")
 let output2 = document.getElementById("passwords")
@@ -6,9 +8,26 @@ let refreshs = document.getElementById("refresh")
 let input = document.getElementById("input") 
 function generate(){
   let password =   Math.floor(Math.random()*numberIndex.length)
-  
   output.textContent += numberIndex[password]
   } 
+  function darkMode(){
+       document.getElementById('body').style.background = '#202938'
+       document.getElementById('container').style.color = '#F5F7FA'
+  }
+  function lightMode(){
+       document.getElementById('body').style.background = '#FFF'
+       document.getElementById('container').style.color = '#333'
+  }
+  darkBtn.addEventListener("click", function(){
+       darkMode()
+       lightBtn.style.display = 'block'
+       darkBtn.style.display = 'none'
+  })
+  lightBtn.addEventListener("click", function(){
+       lightMode()
+       lightBtn.style.display = 'none'
+       darkBtn.style.display = 'block'
+  })
   function generate2(){
   let password1 =   Math.floor(Math.random()*numberIndex.length)
   
@@ -34,8 +53,8 @@ function refresh() {
 }
           const copyContent = async () => {
     try {
-      await navigator.clipboard.writeText(output.textContent);
-      alert('Content copied to clipboard');
+    await navigator.clipboard.writeText(output.textContent);
+      alert(`${output.textContent} copied`);
     } catch (err) {
       console.error('Failed to copy: ', err);
     }
